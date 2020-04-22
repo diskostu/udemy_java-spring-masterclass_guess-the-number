79d7cb013698942d1d0d7f63894ecde21bed7f06
Denny Kluge <denny.kluge@gmail.com>
2020-04-22 21:12:06 +0200

    finished chapter 24 (using a spring container)

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,13 +2,29 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    private static final String CONFIG_LOCATION = "beans.xml";
 
 
     public static void main(String[] args) {
-        LOG.info("Guess the number game");
+        log.info("Guess the number game");
+
+        // create the context
+        final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        // get the bean
+        final NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        final int number = numberGenerator.next();
+        log.info("number = " + number);
+
+        // close the context
+        context.close();
     }
 }
