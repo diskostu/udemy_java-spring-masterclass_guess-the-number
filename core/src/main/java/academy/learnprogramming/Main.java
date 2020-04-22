7d13a288f82e649f582af4862e86c832846aa376
Denny Kluge <denny.kluge@gmail.com>
2020-04-22 20:45:04 +0200

    finished chapter 23 (project setup)

---
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+
+    public static void main(String[] args) {
+        LOG.info("Guess the number game");
+    }
+}
