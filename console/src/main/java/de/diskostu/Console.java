90a7583ee0bffe6b136944a278f305e000f86b14
Denny Kluge <denny.kluge@gmail.com>
2020-04-30 20:26:58 +0200

    inside of chapter 37
    application events
    one way of listening to events: implementing ApplicationListener

---
+++ console/src/main/java/de/diskostu/Console.java
@@ -0,0 +1,19 @@
+package de.diskostu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Console implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger log = LoggerFactory.getLogger(Console.class);
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        log.info("Container ready for use.");
+    }
+}
