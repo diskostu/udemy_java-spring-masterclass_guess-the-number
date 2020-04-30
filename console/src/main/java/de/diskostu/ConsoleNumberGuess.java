97f2d395741dbc0c0dec83ef0edc363b89a9435b
Denny Kluge <denny.kluge@gmail.com>
2020-04-30 21:02:51 +0200

    finished chapter 38
    finish game logic

---
+++ console/src/main/java/de/diskostu/ConsoleNumberGuess.java
@@ -0,0 +1,79 @@
+package de.diskostu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
+public class ConsoleNumberGuess {
+
+    // == constants ==
+    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
+
+    // == fields ==
+    private final Game game;
+
+    private final MessageGenerator messageGenerator;
+
+
+    // == constructor ==
+    @Autowired
+    public ConsoleNumberGuess(Game aGame, MessageGenerator aMessageGenerator) {
+        this.game = aGame;
+        this.messageGenerator = aMessageGenerator;
+    }
+
+
+    /**
+     * When using the {@link EventListener} annotation, the method name is free to choose.
+     * <p>
+     * Option 1: use the type of the event as a method parameter.
+     * </p>
+     */
+    @EventListener(ContextRefreshedEvent.class)
+    public void onContextRefreshed() {
+        log.info("event: Context refreshed.");
+
+        final Scanner scanner = new Scanner(System.in);
+        while (true) {
+            log.info(messageGenerator.getMainMessage());
+            log.info(messageGenerator.getResultMessage());
+
+            final int guess = scanner.nextInt();
+            scanner.nextLine();
+            game.setGuess(guess);
+            game.check();
+
+            if (game.isGameWon() || game.isGameLost()) {
+                log.info(messageGenerator.getResultMessage());
+                log.info("Play again y/n ?");
+
+                final String playAgainString = scanner.nextLine().trim();
+                if (!playAgainString.equalsIgnoreCase("y")) {
+                    break;
+                }
+
+                game.reset();
+            }
+        }
+    }
+
+
+    /**
+     * When using the {@link EventListener} annotation, the method name is free to choose.
+     * <p>
+     * Option 2: use the type of the event as a parameter for the annotation. This is useful if we don't need
+     * the event as a method parameter.
+     * </p>
+     */
+    @EventListener(ContextClosedEvent.class)
+    public void onContextClosed() {
+        log.info("event: Context closed.");
+    }
+}
