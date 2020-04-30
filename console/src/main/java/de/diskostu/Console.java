93e283c1965cdd74fd327afaf9b0b9a2102d40bf
Denny Kluge <denny.kluge@gmail.com>
2020-04-30 20:47:22 +0200

    inside of chapter 37
    application events
    another way of listening to events: using the annotation @EventListener

--- console/src/main/java/de/diskostu/Console.java
+++ console/src/main/java/de/diskostu/Console.java
@@ -2,18 +2,40 @@ package de.diskostu;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Console implements ApplicationListener<ContextRefreshedEvent> {
+public class Console {
 
     private static final Logger log = LoggerFactory.getLogger(Console.class);
 
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        log.info("Container ready for use.");
+    /**
+     * When using the {@link EventListener} annotation, the method name is free to choose.
+     * <p>
+     * Option 1: use the type of the event as a method parameter.
+     * </p>
+     *
+     * @param event the event to listen for
+     */
+    @EventListener
+    public void onContextRefreshed(ContextRefreshedEvent event) {
+        log.info("event: Context refreshed.");
+    }
+
+
+    /**
+     * When using the {@link EventListener} annotation, the method name is free to choose.
+     * <p>
+     * Option 2: use the type of the event as a parameter for the annotation. This is useful if we don't need
+     * the event as a method parameter.
+     * </p>
+     */
+    @EventListener(ContextClosedEvent.class)
+    public void onContextClosed() {
+        log.info("event: Context closed.");
     }
 }
