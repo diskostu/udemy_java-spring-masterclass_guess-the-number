99bd437d724b5dae4ffdba4e4acfa3fdd3a53639
Denny Kluge <denny.kluge@gmail.com>
2020-05-07 21:51:10 +0200

    finished chapter 47
    using lombok

--- core/src/main/java/de/diskostu/GameImpl.java
+++ core/src/main/java/de/diskostu/GameImpl.java
@@ -1,29 +1,33 @@
 package de.diskostu;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@Slf4j
+@Getter
 @Component
 public class GameImpl implements Game {
 
-    // == constants ==
-    private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
-
     // == fields ==
+
+    @Getter(AccessLevel.NONE) // we don't want a getter for this field
     private final NumberGenerator numberGenerator;
     private final int guessCount;
-
     private int number;
-    private int guess;
     private int smallest;
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
+
+    @Setter // we need a setter for this field
+    private int guess;
 
 
     @Autowired
@@ -69,54 +73,6 @@ public class GameImpl implements Game {
         }
 
         remainingGuesses--;
-    }
-
-
-    @Override
-    public int getNumber() {
-        return number;
-    }
-
-
-    @Override
-    public int getGuess() {
-        return guess;
-    }
-
-
-    @Override
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-
-
-    @Override
-    public int getSmallest() {
-        return smallest;
-    }
-
-
-    @Override
-    public int getBiggest() {
-        return biggest;
-    }
-
-
-    @Override
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-
-
-    @Override
-    public int getGuessCount() {
-        return guessCount;
-    }
-
-
-    @Override
-    public boolean isValidNumberRange() {
-        return validNumberRange;
     }
 
 
